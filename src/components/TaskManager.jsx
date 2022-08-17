df145833c956983f08e9a51b8c3d8e55b44ff806
Josephine Nana Yaa Asare <nanayaaasare18@gmail.com>
2022-08-17 14:22:10 -0700

    useState

--- src/components/TaskManager.jsx
+++ src/components/TaskManager.jsx
@@ -1,31 +1,51 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import TaskItem from "./TaskItem";
+import {v4 as uuid} from "uuid"
 
 
-function TaskManager() {
-    const [tasks, setTasks] = useState([]);
-    const [input, setInput] =useState("");
+    const getTasksFromLocalStorage = () => {
+        // get tasks from local storage
+     const tasks = localStorage.getItem("tasks")
+     if (!tasks) return []
+     return JSON.parse(tasks)
+    }
 
-    const handleSubmit = (e) => {
+    function TaskManager(){
+        const [tasks, setTasks] = useState(getTasksFromLocalStorage);
+        const [input, setInput] = useState("");
+    
+
+     const handleSubmit = (e) => {
         e.preventDefault();
         console.log("Form Submitted")
         if (input === "") return;
 
-        setTasks([input, ...tasks]);
+        const newTask ={
+            id: uuid(),
+            text: input,
+            completed: false,
+        };
+
+        setTasks([newTask, ...tasks]);
         setInput("")
     }
 
-        const handleDelete = (idx) => {
-          const newTasks = tasks.filter((task) => task !== idx)
+        const handleDelete = (id) => {
+          const newTasks = tasks.filter((task) => task.id !== id)
           setTasks(newTasks);
-          }
+        }
     
+        useEffect(() => {
+            localStorage.setItem("tasks", JSON.stringify(tasks)); 
+        }, [tasks])
+        
+        // map runs through the items in the arrays, map returns array but loop
         return( 
        <div className="h-screen w-screen bg-blue-400 flex justify-center items-center">
             <div className="max-w-xl bg-white rounded-xl px-5 py-10">
                 <form 
                    className="space-x-5 flex w-[30rem] mb-10"
-                    onSubmit={handleSubmit}>  
+                    onSubmit={handleSubmit} > 
                     <input type="text" className="border-2 border-green-400 p-2 rounded-md outline-none w-10/12"
                      onChange={(e) => setInput(e.target.value)}
                      value={input} />
@@ -36,8 +56,8 @@ function TaskManager() {
                    </form>
 
                      <div className="space-y-2 overflow-auto h-56">
-                    {tasks.map((task) => (
-                         <TaskItem task={task} handleDelete={handleDelete} />
+                       {tasks.map((task) => (
+                         <TaskItem key={task.id} task={task} handleDelete={handleDelete} />
                       
                     ))}
                 
@@ -48,4 +68,5 @@ function TaskManager() {
         </div>
     )
 }
-export default TaskManager
+
+export default TaskManager;
